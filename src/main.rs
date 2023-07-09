000b99700ee28a910a185a92cea8e3e48d532c55
SAN <shamoa.daniel@gmail.com>
2023-07-09 11:22:35 +0100

    working stream map example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use futures::{SinkExt, StreamExt}; //, TryFutureExt};
+use futures::{SinkExt, StreamExt, TryFutureExt}; //, TryFutureExt};
 use futures::stream::SplitStream;
 // use tokio::io::AsyncBufReadExt;
 // use tokio::sync::mpsc;
@@ -35,7 +35,7 @@ pub async fn get_binance_snapshot(symbol: &String) -> Result<String> {
     Ok(body)
 }
 
-pub async fn get_binance_stream(symbol: &String) -> Result<WebSocketStream<MaybeTlsStream<TcpStream>>> {
+pub async fn get_binance_stream(symbol: &String) -> Result<SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>> {
     let ws_url_binance = url::Url::parse("wss://stream.binance.us:9443")
     .context("wrong binance url")?
     .join(&format!("/ws/{}@depth20@100ms", symbol))?;
@@ -43,11 +43,14 @@ pub async fn get_binance_stream(symbol: &String) -> Result<WebSocketStream<Maybe
     let (ws_stream_binance, _) = connect_async(&ws_url_binance)
     .await
     .context("Failed to connect to binance wss endpoint")?;
-    Ok(ws_stream_binance)
+
+    let (_, read_stream) = ws_stream_binance.split();
+
+    Ok(read_stream)
 }
 
 
-pub async fn get_bitstamp_stream(symbol: &String) -> Result<WebSocketStream<MaybeTlsStream<TcpStream>>> {
+pub async fn get_bitstamp_stream(symbol: &String) -> Result<SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>> {
     let ws_url_bitstamp = url::Url::parse("wss://ws.bitstamp.net")
     .context("wrong bitstamp url")?;
 
@@ -64,42 +67,59 @@ pub async fn get_bitstamp_stream(symbol: &String) -> Result<WebSocketStream<Mayb
     println!("{}", subscribe_msg);
 
     ws_stream_bitstamp.send(Message::Text(subscribe_msg.to_string())).await.unwrap();
+    //ws_stream_bitstamp.next();
 
     println!("sent subscription message");
+    let (_, read_stream) = ws_stream_bitstamp.split();
+    // read_stream.next();
 
-    Ok(ws_stream_bitstamp)
+    Ok(read_stream)
 }
 
 // TODO: do this full implementation
 pub async fn get_all_streams(symbol: String) -> Result<StreamMap<String, SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>>> {
-    let streams_map = StreamMap::new();
+    let mut streams_map = StreamMap::new();
+
+    let binance_stream_read = get_binance_stream(&symbol).await.unwrap();
+    streams_map.insert("BINANCE".to_string(), binance_stream_read);
+
+
+    let bitstamp_stream_read = get_bitstamp_stream(&symbol).await.unwrap();
+    streams_map.insert("BITSTAMP".to_string(), bitstamp_stream_read);
+
+    println!("all streams returning");
+
     Ok(streams_map)
 }
 
 
 #[tokio::main]
 async fn main() -> Result<()>{
-    // careful with binance, apparently btcusd is not btcusd but the correct ticker is btcusd
+    // careful with binance, apparently btcusd is not btcusd but the correct ticker is btcusdt
     let symbol = "btcusdt".to_string();
     
     // This works
-    // let ws_stream = get_bitstamp_stream(&symbol).await.context("Error in getting bistamp stream").unwrap();
+    // let ws_read_stream = get_bitstamp_stream(&symbol).await.context("Error in getting bistamp stream").unwrap();
 
-    let ws_stream = get_binance_stream(&symbol).await.context("Error in getting bistamp stream").unwrap();
-    
-    let (_, read) = ws_stream.split();
+    // let ws_read_stream = get_binance_stream(&symbol).await.context("Error in getting bistamp stream").unwrap();
+    let mut stream_map = get_all_streams(symbol).await.unwrap();
+    while let Some((key, val)) = stream_map.next().await {
+        println!("UPDATE RECEIVED");
+        println!("{}", key);
+        println!("{}", val.unwrap().into_text().unwrap());
+    }
 
-    let read_future = read.for_each(|message| async {
-        println!("receiving...");
-        let unwrapped_message = message.unwrap();
-         //let data = unwrapped_message.into_data();
-         let msg_str = unwrapped_message.into_text().unwrap();
-         // tokio::io::stdout().write(&data).await.unwrap();
-         println!("{}", msg_str);
-         println!("received...");
-    });
+    // let read_future = stream_map.for_each(|message| async {
+    //     println!("receiving...");
+    //     let unwrapped_message = message.unwrap();
+    //      //let data = unwrapped_message.into_data();
+    //      let msg_str = unwrapped_message.into_text().unwrap();
+    //      // tokio::io::stdout().write(&data).await.unwrap();
+    //      println!("{}", msg_str);
+    //      println!("received...");
+    // });
 
-    read_future.await;
+    // read_future.await;
 
     Ok(())
 }
